af26393606d0e04b9255e6b07231899973947bc8
Felice Forby <feliceforby@gmail.com>
2018-05-01 22:51:57 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,49 @@
+class Song
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre 
+    @@count += 1
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    unique_genres = []
+    @@genres.each{ |genre| unique_genres << genre unless unique_genres.include?(genre) }
+    unique_genres
+  end
+  
+  def self.genre_count
+    genre_histogram = {}
+    @@genres.each do |genre|
+      genre_histogram.keys.include?(genre) ? genre_histogram[genre] += 1 : genre_histogram[genre] = 1
+    end
+    genre_histogram
+  end
+  
+  def self.artists
+    unique_artists = []
+    @@artists.each{ |artist| unique_artists << artist unless unique_artists.include?(artist) }
+    unique_artists
+  end
+  
+  def self.artist_count
+    artist_histogram = {}
+    @@artists.each do |artist|
+      artist_histogram.keys.include?(artist) ? artist_histogram[artist] += 1 : artist_histogram[artist] = 1
+    end
+    artist_histogram
+  end
+  
+end
